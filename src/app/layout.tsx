ec33a8c0d1f79ed920fc84186b0e0eb516aa4755
Leladatan <leladatan@yandex.ru>
2025-02-28 13:32:53 +0300

    Refactor: Изменил SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,44 @@ import Header from "../widgets/header";
 const inter: NextFont = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-    title: "TeRoNet",
-    description: "Свяжитесь с работодателями и соискателями",
+    title: "TeRoNet | Business Practice",
+    description:
+        "TeRoNet - платформа для поиска работы и найма специалистов в IT. Свяжитесь с работодателями и соискателями, публикуйте вакансии и находите лучшие таланты.",
+    keywords: [
+        "работа в IT",
+        "поиск сотрудников",
+        "вакансии IT",
+        "найм в IT",
+        "IT рекрутинг",
+        "TeRoNet",
+        "технологии",
+        "карьера в IT",
+    ],
+    openGraph: {
+        title: "TeRoNet: Найдите работу и сотрудников в сфере технологий",
+        description:
+            "TeRoNet - платформа для поиска работы и найма специалистов в IT. Свяжитесь с работодателями и соискателями, публикуйте вакансии и находите лучшие таланты.",
+        url: "https://teronet.vercel.app/",
+        siteName: "TeRoNet",
+        images: [
+            {
+                url: "https://teronet.vercel.app/images/teronet-logo.png",
+                width: 800,
+                height: 600,
+            },
+        ],
+        locale: "ru_RU",
+        type: "website",
+    },
+    robots: {
+        index: true,
+        follow: true,
+        nocache: false,
+        googleBot: {
+            index: true,
+            follow: true,
+        },
+    },
 };
 
 const RootLayout = ({children}: Readonly<{ children: React.ReactNode }>) => {
